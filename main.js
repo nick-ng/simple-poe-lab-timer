32beafc6c6fa2f6d777fcc6decd74bf8d2984325
Nick Ng <nicholas.cj.ng@gmail.com>
2017-10-01 18:55:38 +1300

    removed developer mode flag.x

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const path = require('path')
 const url = require('url')
 const narwhalIconPath = path.join(__dirname, 'favicon.ico');
 
-const devMode = true;
+const devMode = false;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
